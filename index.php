e57388f14c81c8bae44150573de43a6141e75951
westin <westin007@gmail.com>
2015-01-27 18:50:16 -0600

    Bootstrap changes - Mr.B

--- index.php
+++ index.php
@@ -7,14 +7,35 @@ global $conn;
 <html>
     <head>
 	<link href="bootstrap-3.3.1-dist/dist/css/bootstrap.min.css" rel="stylesheet">
+	<link href="theme.css" rel="stylesheet">
     </head>
-    <body>
-		<script src="bootstrap-3.3.1-dist/dist/js/bootstrap.min.js"></script>
-    <a href="populate_database.php">Create Spell</a> <br>
-    <h1>The spells</h1>
-    <?php
-        for($i = 0; $i<count($spells);$i++)
-        echo "<a href='populate_database.php?spell_id=".$spells[$i][0]."'>".$spells[$i][1]."</a><br>"
-    ?>
+	<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+		<div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Spell Search</a>
+        </div>
+		<div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href=".">Home</a></li>
+            <li><a href="populate_database.php">Add</a></li>
+            <li><a href="index.php">Library</a></li>
+          </ul>
+        </div>
+	</nav>
+    <body role="document">
+		<div class="container theme-showcase" role="main"> 
+			<script src="bootstrap-3.3.1-dist/dist/js/bootstrap.min.js"></script>
+			
+			<h1>The spells</h1>
+			<?php
+				for($i = 0; $i<count($spells);$i++)
+				echo "<a href='populate_database.php?spell_id=".$spells[$i][0]."'>".$spells[$i][1]."</a><br>"
+			?>
+		</div>
     </body>
 </html>
