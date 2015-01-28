ee352a4138ddd37e5cbbe8eaeaa8b6eb6c3b5bbf
westin <westin007@gmail.com>
2015-01-27 18:46:31 -0600

    Continuing scrubbed db - Mr.B

--- connect_database.php
+++ connect_database.php
@@ -1,8 +1,15 @@
 <?php
+<<<<<<< HEAD
 $servername = "x";
 $username = "y";
 $password = "z";
 $database_name = "db";
+=======
+$servername = "localhost";
+$username = "webuser";
+$password = "3.1415isGood";
+$database_name = "db554888958";
+>>>>>>> 84a90a1... Changing db details because COME ON - Mr.B
 $database_port = 3306;
 // Create connection
 $conn = new mysqli($servername, $username, $password,$database_name,$database_port);
